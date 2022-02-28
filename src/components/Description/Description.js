c4a86680fe40dba0888cb28c358c2e856859c60b
mohammad salehi <mohammad7979salehi@gmail.com>
2022-02-28 23:59:56 +0330

    :)

--- src/components/Description/Description.js
+++ src/components/Description/Description.js
@@ -5,12 +5,12 @@ const Description = () => {
         <div className="mainDiv">
             <div id="DescriptionBox">
                 <h1 className='Dtitle'>what is Pas?</h1>
-                <p className='Dtext'>The purpose of the Pas application is to provide more security for the login process to different sites.<br/>
-                This service provides users with a second dynamic password to login to different sites.<br/>
-                In this service, due to the fact that a variable password is used and that the user must request a password in his user account to receive the password, it is not possible to reveal the passwords and enter the lucky passwords.<br/>
-                For example, banks' second password applications can be used as an example of variable passwords to create more security.<br/>
-                This program uses a dedicated keyboard to login to prevent the browser from saving the password.<br/>
-                Also, all important information is stored encrypted with the hashing and non-decoding process, and there is a guarantee that even with hackers accessing the application database, important user information will not be revealed.<br/>
+                <p className='Dtext'>The purpose of the pas project is to provide more security for the login process on different sites<br/>
+                This project provides a dynamic second password service to different sites<br/>
+                Sites that use this service, their users must log in to their account and receive a 6-digit dynamic password from the special section of that site.<br/>
+                All important information in this service is stored in an encrypted and non-decodable form, and even if the hacker has access to the database, he will not be able to access users' information.<br/>
+                For example, the second password service of banks can be mentioned as an example of using a dynamic second password to increase security.<br/>
+                This is an early version of the app that does not have some of the features of the original version. In the original version, users will use the mobile app and must confirm their mobile number and email.<br/>
                 </p>
                 <h1 className='Dtitle'>Instructions</h1>
                 <h2 className='Dundertitle'>User Signup</h2>
@@ -37,27 +37,25 @@ const Description = () => {
                 </p>
                 <h2 className='Dundertitle'>Site Dashboard</h2>
                 <p className='Dtext'>
-                The first part of the user dashboard is to get the login code to the sites.<br/>
-                In this section, each site is in a separate box with the site address, username and code specific to that site.<br/>
-                By clicking on the password button, users can receive a one-time code with a validity of one minute, which they must enter in the special box for entering the second password on their desired site.<br/>
+                The first part of the dashboard is the password section.<br/>
+                Initially, there is no password for the sites and the user will receive a one-time 6-digit password with a validity of one minute upon request. <br/>
+                </p>
+                <h2 className='Dundertitle'>add users by sites</h2>
+                <p className='Dtext'>
+                Sites can send an array containing their users' usernames to /Addprevioususers to add their previous users.<br/>
+                Also, to add each of their users, send that user name to the /.<br/>
+                Whether to add a group or a single add, after sending the user username, a 7-digit code containing numbers and letters is returned, which sites must show to the user so that the user can add the site to his account.<br/>
                 </p>
                 <h2 className='Dundertitle'>add Site to Account</h2>
                 <p className='Dtext'>
-                To add sites, users must first register on that site.<br/>
-                The site then displays a 6-digit code that they must enter with the site address and username in the "add Site" section.<br/>
-                If all three are correct, the site will be added to the account.<br/>
+                After the user receives the 7-digit code from the desired site, she enters the Add Sites section and enters the site address along with her username on that site and the 7-digit code she received from the same site.<br/>
+                If all items are correct, the site will be added.<br/>
                 </p>
-                <h2 className='Dundertitle'>Site users validation</h2>
+                <h2 className='Dundertitle'>delete Account</h2>
                 <p className='Dtext'>
-                First, users must receive their 6-digit login code from their account in the same site box.<br/>
-                Then enter the code in the desired site and confirm it.<br/>
-                The site then sends the code for validation along with its own token and username to the /confirm address.<br/>
-                Then, after validation, if the validation is confirmed, the status value in the response will send true value to the same site.<br/>
-                </p>
-                <h2 className='Dundertitle'>add previous users</h2>
-                <p className='Dtext'>
-                The site sends a variable with an array consisting of a list of its previous users in the users variable to /Addprevioususers.<br/>
-                The site itself then registers users who have not previously registered.<br/>
+                The user must have registered their email to delete the account.<br/>
+                Then on the main page, enter the account deletion section and enter the email that you have already registered.<br/>
+                In the original version of the program, the delete link is sent to that email, but in the current version, a link is shown to the user and after clicking on it, the account is deleted.<br/>
                 </p>
             </div>
         </div>
